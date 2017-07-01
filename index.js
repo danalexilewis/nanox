bcfa020c2445e7fff82750eda27763f6de229728
Michael Williams <michael.williams@enspiral.com>
2017-07-01 14:09:30 +1200

    more

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-//const h = require('react-hyperscript')
+const h = require('react-hyperscript')
 const randomNumberInRange = require('random-number-in-range')
 
-/*
 function Node (props) {
   const { node } = props
   const { x, y, value, color } = node
@@ -15,6 +14,7 @@ function Node (props) {
   })
 }
 
+/*
 function Flow (props) {
   const { from, to } = props
   const { id: fromId, x: fromX, y: fromY, color: fromColor } = from
@@ -54,6 +54,79 @@ function Flow (props) {
       stroke: `url(#flow)`
     })
   ])
+}
+*/
+
+/*
+function Flow (props) {
+  const { from, to } = props
+  const { id: fromId, x: fromX, y: fromY, color: fromColor } = from
+  const { id: toId, x: toX, y: toY, color: toColor } = to
+  const flowId = `${fromId}-${toId}`
+
+  return <g>
+    <defs>
+      <linearGradient
+        id={flowId}
+        x1={'0%'}
+        y1={'0%'}
+        x2={'100%'}
+        y2={'0%'}
+      >
+        <stop offset={'0%'} stopColor={fromColor} />
+        <stop offset={'100%'} stopColor={toColor} />
+      </linearGradient>
+    </defs>
+    <line
+      x1={fromX}
+      y1={fromY}
+      x2={toX}
+      // https://stackoverflow.com/a/34687362
+      y2={(fromY !== toY) ? toY : toY + 0.0001}
+      strokeWidth={0.02}
+      stroke={`url(#${flowId})`}
+    />
+  </g>
+}
+*/
+
+function Flow (props) {
+  const { from, to } = props
+  const { id: fromId, x: fromX, y: fromY, color: fromColor } = from
+  const { id: toId, x: toX, y: toY, color: toColor } = to
+  const flowId = `${fromId}-${toId}`
+
+  // why can't we use react-hyperscript here?
+
+  return React.createElement('g', {},
+    React.createElement('defs', {},
+      React.createElement('linearGradient', {
+        id: flowId,
+        x1: '0%',
+        y1: '0%',
+        x2: '100%',
+        y2: '0%',
+      },
+        React.createElement('stop', {
+          offset: '0%',
+          stopColor: fromColor
+        }),
+        React.createElement('stop', {
+          offset: '100%',
+          stopColor: toColor
+        })
+      )
+    ),
+    React.createElement('line', {
+      x1: fromX,
+      y1: fromY,
+      x2: toX,
+      // https://stackoverflow.com/a/34687362
+      y2: (fromY !== toY) ? toY : toY + 0.0001,
+      strokeWidth: 0.02,
+      stroke: `url(#${flowId})`
+    })
+  )
 }
 
 function Graph (props) {
@@ -99,71 +172,3 @@ function randomColor () {
   const lightness = '50%'
   return `hsl(${hue}, ${saturation}, ${lightness})`
 }
-*/
-
-function Node (props) {
-  const { node } = props
-  const { x, y, value, color } = node
-  return <circle cx={x} cy={y} r={value} fill={color} />
-}
-
-function Flow (props) {
-  const { from, to } = props
-  const { id: fromId, x: fromX, y: fromY, color: fromColor } = from
-  const { id: toId, x: toX, y: toY, color: toColor } = to
-  const flowId = `${fromId}-${toId}`
-
-  return <g>
-    <defs>
-      <linearGradient
-        id={flowId}
-        x1={'0%'}
-        y1={'0%'}
-        x2={'100%'}
-        y2={'0%'}
-      >
-        <stop offset={'0%'} stopColor={fromColor} />
-        <stop offset={'100%'} stopColor={toColor} />
-      </linearGradient>
-    </defs>
-    <line
-      x1={fromX}
-      y1={fromY}
-      x2={toX}
-      // https://stackoverflow.com/a/34687362
-      y2={(fromY !== toY) ? toY : toY + 0.0001}
-      strokeWidth={0.02}
-      stroke={`url(#${flowId})`}
-    />
-  </g>
-}
-
-function Graph (props) {
-  const source = {
-    id: 'source',
-    x: 0.3,
-    y: 0.5,
-    value: 0.05,
-    color: 'rgb(255, 0, 0)'
-  }
-  const sink = {
-    id: 'sink',
-    x: 0.7,
-    y: 0.5,
-    value: 0.05,
-    color: 'rgb(0, 255, 0)'
-  }
-  return <svg
-    viewBox={'0 0 1 1'}
-    preserveAspectRatio={'none'}
-  >
-  	<Node node={source} />
-    <Flow from={source} to={sink} />
-    <Node node={sink} />
-  </svg>
-}
-
-ReactDOM.render(
-  <Graph />,
-  document.body.querySelector('.main')
-)
